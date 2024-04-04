5ae48abd2c596fe899e034b6ac58f4d3076e00bf
Gideon kirui <kipyegongideon99@gmail.com>
2024-04-04 17:47:33 +0300

    fitlter method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function findMatching(drivers, match) {
+    return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase());
+  }
+  function fuzzyMatch (drivers,query) { 
+    return drivers.filter(driver => driver.startsWith(query));    
+  }
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
